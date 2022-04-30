208d19c4f2ea6e59333afe33ba7843222321bfac
Theodor Abi-Mansour <teddy.am@gmail.com>
2022-05-01 01:38:32 +0300

    Basic Form Finale

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert(['category_name' => 'Cleanings']);
+        DB::table('categories')->insert(['category_name' => 'EPC']);
+        DB::table('categories')->insert(['category_name' => 'EICR']);
+    }
+}
